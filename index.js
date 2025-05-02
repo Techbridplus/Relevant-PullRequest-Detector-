2460a2fe502763fd082f36ae641b8dae23c12175
Gaurav Joshi <118592759+Techbridplus@users.noreply.github.com>
2025-05-02 12:32:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.YOUR_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
 const PORT = process.env.PORT || 3000;
 
